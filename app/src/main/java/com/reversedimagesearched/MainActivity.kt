20090863d95c2042efe1291754ce592a2f71d15d
Awais Mansha <awaismansha1998@gmail.com>
2022-04-09 14:22:01 +0500

    working on it

--- app/src/main/java/com/reversedimagesearched/MainActivity.kt
+++ app/src/main/java/com/reversedimagesearched/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.reversedimagesearched
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import com.reversedimagesearched.util.ManagePermissions
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private val PermissionsRequestCode = 123
+    private lateinit var managePermissions: ManagePermissions
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    companion object {
+        var permissionsList = listOf<String>(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController: NavController = findNavController(R.id.nav_host_fragment)
+
+        //Initialize the bottom navigation view
+        //create bottom navigation view object
+        bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigatin_view)
+        bottomNavigationView.setupWithNavController(navController)
+
+        // Initialize a new instance of ManagePermissions class
+        managePermissions = ManagePermissions(this,permissionsList,PermissionsRequestCode)
+
+        managePermissions.checkPermissions()
     }
+
+    private fun showBottomNav() {
+        bottomNavigationView.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        bottomNavigationView.visibility = View.GONE
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
 }
---
+++ app/src/main/java/com/reversedimagesearched/util/Utility.kt
@@ -0,0 +1,154 @@
+package com.reversedimagesearched.util
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.Matrix
+import android.graphics.drawable.ColorDrawable
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Build
+import android.os.ParcelFileDescriptor
+import android.util.Log
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import androidx.annotation.RequiresApi
+import androidx.core.net.toUri
+import java.io.File
+import java.io.FileDescriptor
+import java.io.FileOutputStream
+import java.io.IOException
+import java.time.LocalDateTime
+
+
+class Utility {
+
+    companion object {
+
+        val LOG_TAG: String = "Utility"
+
+        @SuppressLint("NewApi")
+        fun readUriImage(context: Context, selectedFileUri: Uri): Uri? {
+            try {
+                val parcelFileDescriptor: ParcelFileDescriptor? =
+                    context.getContentResolver()?.openFileDescriptor(selectedFileUri, "r")
+                val fileDescriptor: FileDescriptor = parcelFileDescriptor!!.fileDescriptor
+                val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+                if (parcelFileDescriptor != null) {
+                    parcelFileDescriptor.close()
+                }
+
+                return createDirectoryAndSaveFile(context, image)
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            return null;
+        }
+
+
+        @RequiresApi(Build.VERSION_CODES.O)
+        fun createFileForImage(context: Context): File? {
+            val directoryToStore: File
+            directoryToStore = context.getExternalFilesDir("DukanSoftImages")!!
+            if (!directoryToStore.exists()) {
+                if (directoryToStore.mkdir());
+            }
+            var n = LocalDateTime.now()
+            val fname = "Image-$n.jpg"
+            val file = File(directoryToStore, fname)
+            Log.i(LOG_TAG, "" + file)
+            if (file.exists()) file.delete()
+            file.createNewFile()
+            try {
+                val out = FileOutputStream(file)
+                out.flush()
+                out.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return file
+        }
+
+        @RequiresApi(Build.VERSION_CODES.O)
+        fun createDirectoryAndSaveFile(context: Context, imageToSave: Bitmap): Uri? {
+            val directoryToStore: File
+            directoryToStore = context.getExternalFilesDir("DukanSoftImages")!!
+            if (!directoryToStore.exists()) {
+                if (directoryToStore.mkdir());
+            }
+            var n = LocalDateTime.now()
+            val fname = "Image-$n.jpg"
+            val file = File(directoryToStore, fname)
+            Log.i(LOG_TAG, "" + file)
+            if (file.exists()) file.delete()
+            file.createNewFile()
+            try {
+                val out = FileOutputStream(file)
+                imageToSave.compress(Bitmap.CompressFormat.JPEG, 100, out)
+                out.flush()
+                out.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return file.toUri()
+        }
+
+        fun displayImageInDialog(context: Context,uri: Uri){
+
+            val builder = Dialog(context)
+            builder.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            builder.getWindow()!!.setBackgroundDrawable(
+                ColorDrawable(Color.TRANSPARENT)
+            )
+            builder.setOnDismissListener(DialogInterface.OnDismissListener {
+                //nothing;
+            })
+
+            val bitmapImage = BitmapFactory.decodeFile(uri.path)
+            val nh = (bitmapImage.height * (512.0 / bitmapImage.width)).toInt()
+            val scaled = Bitmap.createScaledBitmap(bitmapImage, 512, nh, true)
+            val imageView = ImageView(context)
+            imageView.setImageBitmap(rotateImageIfRequired(scaled,uri))
+
+            val params = RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            params.setMargins(86,86,86,86)
+            builder.addContentView(
+                imageView, params
+            )
+            builder.show()
+        }
+
+        @Throws(IOException::class)
+        fun rotateImageIfRequired(img: Bitmap, selectedImage: Uri): Bitmap? {
+            val ei = ExifInterface(selectedImage.path!!)
+            val orientation: Int =
+                ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+            return when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(img, 90)
+                ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(img, 180)
+                ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(img, 270)
+                else -> img
+            }
+        }
+
+        fun rotateImage(img: Bitmap, degree: Int): Bitmap? {
+            val matrix = Matrix()
+            matrix.postRotate(degree.toFloat())
+            val rotatedImg = Bitmap.createBitmap(img, 0, 0, img.width, img.height, matrix, true)
+            img.recycle()
+            return rotatedImg
+        }
+
+
+    }
+}
